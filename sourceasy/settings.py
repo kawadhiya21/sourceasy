fa22254ba306175784e1ac61cbe8f9904798f932
Pranay <pranay@sourceasy.co>
2013-12-02 22:54:02 +0530

    testing

--- sourceasy/settings.py
+++ sourceasy/settings.py
@@ -91,8 +91,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config(default='postgres://localhost')
+#import dj_database_url
+#DATABASES['default'] =  dj_database_url.config(default='postgres://localhost')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
