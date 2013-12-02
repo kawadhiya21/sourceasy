14957b8b2685c594d4eed2596ce7b9f1e94b6e36
Pranay <pranay@sourceasy.co>
2013-12-02 17:41:47 +0530

    sourceasy testing 5

--- product/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from product.models import *
-
-class FabricConstants(admin.ModelAdmin):
-    list_display = ('constant_type','constant_value',)
-    list_filter = ('constant_type',)
-admin.site.register(Fabric_Constants,FabricConstants)
-admin.site.register(Product_Fabric)
