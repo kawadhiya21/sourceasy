3585f8dce98ff51da90a65f0a969c97b093f0d2f
Pranay <pranay@sourceasy.co>
2013-12-02 17:33:02 +0530

    sourceasy testing 2

--- sourceasy/settings.py
+++ sourceasy/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -36,7 +36,17 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'product',
+    'south',
 )
+
+TEMPLATE_DIRS = (
+     os.path.join(PROJECT_ROOT, 'templates'),
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -92,8 +102,8 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'static'
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
+STATIC_ROOT = 'statics'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
--- sourceasy/urls.py
+++ sourceasy/urls.py
@@ -1,12 +1,21 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
+# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'sourceasy.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    # url(r'^sourceasy/', include('sourceasy.foo.urls')),
+    url(r'^product/', include('product.urls')),
+    url(r'^users/', include('users.urls')),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^accounts/', include('registration.backends.default.urls')),
+    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
 )
